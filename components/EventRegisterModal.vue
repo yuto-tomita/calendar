c66dbdf8977090539e281f958bed1e3510c40b79
yuto-tomita <qualidea01@gmail.com>
2021-05-26 09:08:55 +0900

    modalの背景をクリックしてもmodalが閉じるように加筆

--- components/EventRegisterModal.vue
+++ components/EventRegisterModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-dark fixed w-full h-full top-0 left-0 bg-opacity-80 flex justify-items-center">
+  <div class="bg-gray-dark fixed w-full h-full top-0 left-0 bg-opacity-80 flex justify-items-center" @click.self="closeModal">
     <div class="bg-white-default mx-auto rounded shadow-lg z-50 overflow-y-auto h-3/6 w-2/3 my-auto relative">
       <div class="absolute right-5 cursor-pointer" @click="closeModal">
         ×
