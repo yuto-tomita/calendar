909bab533281b91012a0676ac5f323416e2dee0d
yuto-tomita <qualidea01@gmail.com>
2021-06-02 20:37:38 +0900

    authのルーティングを追加

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="h-14 shadow relative">
     <div class="flex flex-row-reverse mr-20">
-      <div class="absolute bottom-0 font-bold text-gray-dark cursor-pointer hover:opacity-30">
+      <NuxtLink to="auth" class="absolute bottom-0 font-bold text-gray-dark cursor-pointer hover:opacity-30">
         Login
-      </div>
+      </NuxtLink>
     </div>
   </header>
 </template>
