203ad88229299ffae43f27c6226cae77cc78ece4
yuto-tomita <qualidea01@gmail.com>
2021-06-06 18:02:39 +0900

    Googleログイン処理の記述

--- store/auth.ts
+++ store/auth.ts
@@ -1,18 +1,26 @@
 import { defineStore } from 'pinia'
 import { auth, provider } from '../plugins/firebase'
 
+interface User {
+  email: string
+  name: string
+}
+
 export const authStore = defineStore({
   id: 'auth',
-  state: () => ({
-    email: ''
+  state: ():User => ({
+    email: '',
+    name: ''
   }),
   actions: {
     async login () {
       try {
         const res = await auth.signInWithPopup(provider)
-        console.log(res)
+
+        this.email = res.additionalUserInfo.profile.email
+        this.name = res.additionalUserInfo.profile.name
       } catch (e) {
-        console.log(e)
+        throw new Error('ログインに失敗 ')
       }
     }
   }
