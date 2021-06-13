b3652927524b5dcb73320f01f768b015c93307dd
yuto-tomita <qualidea01@gmail.com>
2021-06-13 20:59:28 +0900

    oauth認証後のcodeをpiniaで管理できることに成功

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,13 +1,25 @@
+import { defineComponent } from "@nuxtjs/composition-api";
+
 <template>
   <div>
     <p>トップページに遷移しない場合、下のリンクをクリックしてください</p>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, useRoute, computed } from '@nuxtjs/composition-api'
+import { calendarApi } from '../store'
 
-}
+export default defineComponent({
+  setup () {
+    const route = useRoute()
+    const api = calendarApi()
+
+    const code = computed(() => route.value.query.code)
+    api.saveCode(code)
+    console.log(api.code)
+  }
+})
 </script>
 
 <style>
--- plugins/calendar.ts
+++ plugins/calendar.ts
@@ -20,13 +20,6 @@ export const connectApi = async () => {
 
   // await axios.post(serviceAccount.token_uri, params)
   //   .then(res => console.log(res))
-  const apiUrl = `
-    https://accounts.google.com/o/oauth2/v2/auth?
-		response_type=code&
-		scope=https://www.googleapis.com/auth/admin.directory.resource.calendar.readonly&
-		include_granted_scopes=true&
-		redirect_uri=http://localhost:3000/callback&
-		client_id=513531115468-qkllk6l03aq2pa2rmcouju8hiu0mefdp.apps.googleusercontent.com
-  `
+  const apiUrl = 'https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=https://www.googleapis.com/auth/calendar&include_granted_scopes=true&redirect_uri=http://localhost:3000/callback&client_id=513531115468-qkllk6l03aq2pa2rmcouju8hiu0mefdp.apps.googleusercontent.com'
   window.location.href = apiUrl
 }
