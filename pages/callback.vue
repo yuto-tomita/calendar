12d3b885b1bf0669c63e94c4875799b2854fbd3a
yuto-tomita <qualidea01@gmail.com>
2021-06-14 22:29:05 +0900

    ログイン成功後5秒後にトップ画面に遷移するように加筆

--- pages/callback.vue
+++ pages/callback.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <p>hoge</p>
+    <p>{{ isAuthSuccess }}</p>
+    <p>{{ timeLimit }}秒後にホーム画面に遷移します</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, useRoute, computed } from '@nuxtjs/composition-api'
+import { defineComponent, useRoute, computed, ref, useRouter } from '@nuxtjs/composition-api'
 import { tokenStore } from '../store'
 
 export default defineComponent({
@@ -13,17 +14,36 @@ export default defineComponent({
     const route = useRoute()
     const store = tokenStore()
     const code = computed(() => route.value.query)
+    const timeLimit = ref<number>(5)
+    const isAuthSuccess = ref<string>('')
+    const router = useRouter()
+
     getAccessToken()
+    fiveCountToTopPage()
 
     async function getAccessToken () {
       try {
         if (typeof code.value.code === 'string') {
           await store.getAccessToken(code.value.code)
+          isAuthSuccess.value = 'ログインに成功しました'
         }
       } catch (e) {
-        console.log(e)
+        isAuthSuccess.value = 'ログインに失敗しました'
       }
     }
+
+    function fiveCountToTopPage () {
+      const timer = setInterval(() => {
+        if (timeLimit.value !== 0) {
+          timeLimit.value = timeLimit.value - 1
+        } else {
+          clearInterval(timer)
+          router.push('/')
+        }
+      }, 1000)
+    }
+
+    return { isAuthSuccess, timeLimit }
   }
 })
 </script>
